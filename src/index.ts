f84f236793e3a3eeac4258929ca392a810227320
James Lew <jamessebastianlew@gmail.com>
2022-07-18 22:36:24 +1000

    updated force https

--- src/index.ts
+++ src/index.ts
@@ -13,16 +13,16 @@ import { readFileSync as read } from 'fs';
 // back-end server
 const app = express();
 
+app.enable('trust proxy');
+app.use(forwardHTTPS);
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use('/api', mainRouter);
 
-const httpApp = express();
-httpApp.get('*', forwardHTTPS);
-
-const httpServer = http.createServer();
+const httpServer = http.createServer(app);
 const httpsServer = https.createServer({
     key: read(path.join(__dirname, 'keys/privkey.pem')),
     cert: read(path.join(__dirname, 'keys/fullchain.pem'))
@@ -37,7 +37,6 @@ const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/pondoexchange";
 const main = async () => {
     await mongoose.connect(DB_URL);
 
-    // TODO: add https certs and redirect http to https
     if (process.env.ALLOW_HTTP)
         httpServer.listen(PORT, () => console.log(`http server running on port ${PORT}`));
     httpsServer.listen(SECURE_PORT, () => console.log(`https server running on port ${SECURE_PORT}`));
--- src/utils/forward-https.ts
+++ src/utils/forward-https.ts
@@ -1,5 +1,15 @@
 import { RequestHandler } from 'express';
 
-export const forwardHTTPS: RequestHandler = (req, res) => {
-    return res.redirect('https://' + req.headers.host + req.url);
+export const forwardHTTPS: RequestHandler = (req, res, next) => {
+    if (req.headers.host === undefined) {
+        const error = new Error('missing host in header');
+        return next({ error });
+    }
+
+    if (!req.secure) {
+        const new_host = req.headers.host.split(':')[0] + ':8443';
+        return res.redirect("https://" + new_host + req.url);
+    }
+
+    next();
 };
